c1d75e53542d2884eb2176baa3ff6dc218904aec
charholt <charity.holt@icloud.com>
2019-08-09 16:10:54 -0400

    Remove Require token in PATCH Removed require token in PATCh request

--- app/routes/event_routes.js
+++ app/routes/event_routes.js
@@ -76,7 +76,7 @@ router.post('/events', requireToken, (req, res, next) => {
 
 // UPDATE
 // PATCH /events/5a7db6c74d55bc51bdf39793
-router.patch('/events/:id', requireToken, removeBlanks, (req, res, next) => {
+router.patch('/events/:id', removeBlanks, (req, res, next) => {
   // if the client attempts to change the `owner` property by including a new
   // owner, prevent that by deleting that key/value pair
   delete req.body.event.owner
